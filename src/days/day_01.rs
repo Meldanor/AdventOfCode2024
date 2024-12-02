333318b61af312f4eacc6f455a697251b28e9d2d
Kilian Gärtner <contact@kilian-gaertner.dev>
2024-12-02 12:22:45 +0100

    feat: solve day 01 part 01

--- src/days/day_01.rs
+++ src/days/day_01.rs
@@ -1,4 +1,25 @@
 pub fn run(input: &Vec<String>) {
     println!("Hello, this is Day 01");
-    println!("{}", input.join("\n"));
+
+    let mut distance_sum: u32 = 0;
+    let (left_list, right_list) = parse_lists(input);
+    for (i, left_location_id) in left_list.iter().enumerate() {
+        let right_location_id = right_list[i];
+        distance_sum += left_location_id.abs_diff(right_location_id);
+    }
+    println!("The sum of distances is {}", distance_sum);
+}
+
+fn parse_lists(input: &Vec<String>) -> (Vec<u32>, Vec<u32>) {
+    let mut left_list: Vec<u32> = Vec::new();
+    let mut right_list: Vec<u32> = Vec::new();
+
+    for line in input.iter() {
+        let values: Vec<&str> = line.split_whitespace().collect();
+        left_list.push(values[0].parse().unwrap());
+        right_list.push(values[1].parse().unwrap());
+    }
+    left_list.sort();
+    right_list.sort();
+    return (left_list, right_list);
 }
